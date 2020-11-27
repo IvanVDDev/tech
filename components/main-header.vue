67b19eb05ac36f7bb7e372991a59878bb8e84f5d
Ivan Vukadinovic <ivanv@lgs036.com>
2020-11-27 13:50:56 +0100

    Set header layout

---
+++ components/main-header.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="w-screen px-10 py-10">
+    <header class="container mx-auto flex flex-row">
+        <div class="w-1/4">LOGO</div>
+        <!-- ... -->
+        <nav class="w-3/4"> 
+          <ul  class="flex flex-row justify-end">
+            <li class="px-10"><a href="#">Home</a></li>
+            <li class="px-10"><a href="#">Home</a></li>
+            <li class="px-10"><a href="#">Home</a></li>
+            <li class="pl-10"><a href="#">Home</a></li>
+          </ul>
+        </nav>
+    </header>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
