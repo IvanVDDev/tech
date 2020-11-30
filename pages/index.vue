17901bf1aff7ef7354a08e423ef78171c37fe05d
Ivan Vukadinovic <ivanv@lgs036.com>
2020-11-30 13:45:02 +0100

    Insert landing page hero section

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,16 @@
 <template>
   <div>
   <main-header/>
+  <div class="s px-10 py-100 hero-section hero-section">
+    <div class="container mx-auto flex flex-row justify-center items-center">
+      <div class="w-1/2 hero-text">
+        <h1>Some text that should leave an impression</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, quae harum. Nisi aliquam quasi illum dignissimos vel, totam autem eos modi consectetur necessitatibus perferendis, expedita earum iusto reiciendis fugiat aperiam.</p>
+      <button>Go for it!</button>
+      </div>
+      <div class="w-1/2 hero-image"><img src="~/assets/images/hero.png" alt=""></div>
+    </div>
+  </div>
   </div>
 </template>
 
@@ -11,3 +21,49 @@ export default {
   components: { MainHeader },}
 </script>
 
+<style lang="scss">
+html {
+  font-family: 'Montserrat';
+}
+
+.hero-image {
+  margin-top: 20px; 
+}
+
+.hero-section {
+  padding-top: 80px;
+  overflow: hidden;
+  min-height: 100vh;
+  background: rgb(58,62,180);
+  background: linear-gradient(149deg, rgba(58,62,180,1) 0%, rgba(253,29,229,1) 94%);
+
+  h1 {
+  font-size: 50px;
+  color: white;
+  font-weight: 700;
+  margin-bottom: 23px;
+  }
+
+  p {
+    color: white;
+    max-width: 80%;
+    margin-bottom: 55px;
+  }
+
+  button {
+     background-color: white;
+    padding: 20px 40px;
+    border-radius: 15px;
+    font-size: 20px;
+    color: rgb(58,62,180);
+    font-weight: 700;
+    transition: all 0.2s ease-in-out;
+    box-shadow: 0px 0px 15px 2px rgb(58,62,180);
+
+    &:hover {
+      background-color:  rgba(253,29,229,1);
+      color: white;
+    }
+  }
+}
+</style>
