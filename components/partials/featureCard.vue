5dbb18723c9e03010c4c166986b52a9897fe0dd6
Ivan Vukadinovic <ivanv@lgs036.com>
2020-12-02 17:01:01 +0100

    Fix feature card image render bug

--- components/partials/featureCard.vue
+++ components/partials/featureCard.vue
@@ -2,7 +2,7 @@
   <div class="w-1/3">
           <div class="feature-card flex flex-col items-center rounded-lg shadow-lg  mx-8">
               <div>
-                <img :src="feature.image" alt="">
+                <img :src="getImgUrl(feature.image)" alt="">
               </div>
               <div class="py-10 px-10">
                 <h3 class="mb-10 mt-6">{{feature.title}}</h3>
@@ -21,6 +21,12 @@ export default {
     data() {
         return {
             feature : this.featureInfo
+        }
+    },
+
+    methods: {
+        getImgUrl: (pic) => {
+            return require('@/assets/images/'+pic)
         }
     }
 }
--- data/features.js
+++ data/features.js
@@ -1,16 +1,16 @@
 export default [
     {
-        image:"/_nuxt/assets/images/desktop.jpg",
+        image:"desktop.jpg",
         title:"Web development",
         description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, expedita?"
     },
     {
-        image:"/_nuxt/assets/images/web.jpg",
+        image:"web.jpg",
         title:"Desktop & mobile apps",
         description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, expedita?"
     },
     {
-        image:"/_nuxt/assets/images/seo.jpg",
+        image:"seo.jpg",
         title:"Search engine optimization",
         description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, expedita?"
     }
